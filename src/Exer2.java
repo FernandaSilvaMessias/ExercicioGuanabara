c7207ce7c1089d4fbe03c8eb809e86f5d5f91c54
Fernanda Silva <cassia.messias.dev@gmail.com>
2024-05-05 21:46:44 -0300

    Exercicio Guanabara

---
+++ src/Exer2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exer2 {
+
+	public static void main(String[] args) {
+		//2) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-
+		//vindas para ela:
+			//Ex:
+			//Qual é o seu nome? João da Silva
+			//Olá João da Silva, é um prazer te conhecer!		
+		Scanner scan = new Scanner (System.in);
+		String nome;
+		System.out.println("Digite seu nome");
+		
+		System.out.println(nome);
+		String nome= scan.nextInt();
+	}
+
+}
